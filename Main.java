786dce37a23b9908d9eb60be9e890340a6d1991c
trwinowiecki <trwinowiecki@KL202HELP124.admnet.oakland.edu>
2017-07-27 13:07:12 -0400

    whatevs

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
 		// Start GUI
-		
+		MainGUI gui = new MainGUI();
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(gui);
+        frame.pack();
+        frame.setVisible(true);
 	}
 }
