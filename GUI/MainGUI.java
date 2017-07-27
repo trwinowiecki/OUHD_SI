9399cdf878670a907ef83f465ddd59e5e7f0643f
trwinowiecki <trwinowiecki@KL202HELP124.admnet.oakland.edu>
2017-07-27 12:53:42 -0400

    gui

--- GUI/MainGUI.java
+++ GUI/MainGUI.java
@@ -1,17 +1,5 @@
-import javax.swing.JFrame;
-import java.awt.*;
+package GUI;
 
-public class RectangleProgram extends JFrame
-{
-	private static final int WIDTH = 400;
-	private static final int HEIGHT = 300;
-	
-	public RectangleProgram()
-	{
-		setTitle("Sample Title: Area of a Rectangle");
-		setSize(WIDTH, HEIGHT);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-	
+public class MainGUI {
+
 }
